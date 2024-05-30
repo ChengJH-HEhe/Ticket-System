d217f862b1d76a27da977273978c1082d242101d
OranJun <Cheng_Junhong_2023@sjtu.edu.cn>
2024-05-30 11:38:13 +0800

    shrink size

--- include/train.hpp
+++ include/train.hpp
@@ -81,7 +81,7 @@ struct TrainManager {
   Bptree::BPlusTree<pair<unsigned int,int>, int, unsigned int> TrainID;
   Bptree::BPlusTree<pair<int,int>, int, int> release;
   // from stations to the id price time
-  Bptree::BPlusTree<pair<unsigned int, TrainInfo>, TrainInfo, unsigned int, 1600> seat;
+  Bptree::BPlusTree<pair<unsigned int, TrainInfo>, TrainInfo, unsigned int, 1280> seat;
   // steal? find second -intmax min
   FileManager<Train> TrainFile;
   FileManager<Reinfo> ReFile;
